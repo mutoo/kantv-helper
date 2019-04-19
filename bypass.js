33580ee2a2b4cba4cfd6d7e9d2d9b8e16ef08c01
Lingjia Liu <gmutoo@gmail.com>
2019-04-19 23:36:19 +1000

    20190419

--- bypass.js
+++ bypass.js
@@ -1,35 +1,45 @@
 (function() {
-    var jq;
-
-    try {
-        if (!window.webpackJsonp) {
-            throw new Error('Could not retrieve the module from this bundle.');
-        }
-        jq = window.webpackJsonp([], [], ['7t+N']);
-
-        if (!jq) {
-            throw new Error('Could not retrieve jquery by given moduleId.');
-        }
-
-        if (!jq.ajaxPrefilter) {
-            throw new Error('Could not found ajaxPrefilter function in jq Object.');
-        }
-    } catch (e) {
-        console.warn(e + ' Try using window.jQuery.');
-        jq = window.jQuery;
+    // skip ad
+    let ad = document.querySelector('#vjs-mandatory-advertisement');
+    let adVue = ad.__vue__;
+    if (!adVue) {
+        return 'ad vue is not detected.';
     }
+    if (adVue.currentAdvertisingIndex !== -1) {
+        if (adVue.advertisingTimer)
+            clearInterval(adVue.advertisingTimer);
+        adVue.endOfAdvertisement();
+    } else {
+        console.log('No ad detected.');
+    }
+})();
 
-    jq.ajaxPrefilter(function(opt) {
-        if (opt.url == window.currentPartInfo.url.wxSubscribe) {
-            var success = opt.success;
-            opt.success = function(res) {
-                if (res && res.data) {
-                    res.data.subscribe = 1;
-                } else {
-                    console.error('Could not manipulate the response.');
-                }
-                success(res);
-            };
-        }
+(function() {
+    // skip qrcode
+    let qr = document.querySelector('#vjs-qr-code');
+    let qrVue = qr.__vue__;
+    if (!qrVue) {
+        return 'qr vue is not detected.';
+    }
+    qrVue.followedSuccess = true;
+    qrVue.ajaxOnOff = true;
+    qrVue.$emit('update:displayState', false);
+    qrVue.$emit('update:coverAndProhibitPlay', false);
+    qrVue.initState.end || qrVue.$emit('update:initState', {
+        start: true,
+        end: true,
+        skipOtherOptions: true,
     });
+    clearTimeout(qrVue.requestSubscribeTimer);
+
+    // force to hide
+    qrVue.$el.style.display = 'none';
+})();
+
+(function() {
+    // hide pause ad
+    var styleEl = document.createElement('style');
+    document.head.appendChild(styleEl);
+    var styleSheet = styleEl.sheet;
+    styleSheet.insertRule('#vjs-pause-advertising{display:none!important;}');
 })();
